20fe8b3e8c6e78b83f28e5e993714d63cb7a0205
TaechaPR <taechapatr@gmail.com>
2025-02-13 17:38:40 +0700

    11

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,7 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+  // Other configurations...
+};
